7eb4531e7cadec5d7a084b94037ef7269995d8a3
Thomas LECOUFFE <thomaslecouffe@hotmail.fr>
2019-05-14 13:32:46 +0200

    Implémentation de la class instruments

--- SFMLTest02/Instrument.cpp
+++ SFMLTest02/Instrument.cpp
@@ -1,10 +1,40 @@
 #include "Instrument.h"
+#include "Notes.h"
+#include <map>
 
-Instrument::Instrument()
+Instrument::Instrument(std::string name)
 {
+	nameInstrument = name;
 }
 
+Instrument::Instrument(std::string name, std::map<int, Notes> tabNotes)
+{
+	nameInstrument = name;
+	listNotes = tabNotes;
+}
 
 Instrument::~Instrument()
 {
+}
+
+void Instrument::addNotes(int emp, Notes noteSup)
+{
+	listNotes.insert(std::pair<int, Notes>(emp, noteSup));
+}
+
+void Instrument::addNotes(int emp, std::string noteFile)
+{
+	Notes noteSup(noteFile);
+	listNotes.insert(std::pair<int, Notes>(emp, noteSup));
+}
+
+void Instrument::addNotes(int emp, sf::SoundBuffer noteBuff)
+{
+	Notes noteSup(noteBuff);
+	listNotes.insert(std::pair<int, Notes>(emp, noteSup));
+}
+
+void Instrument::play(int emp)
+{
+	listNotes[emp].play();
 }
--- SFMLTest02/Instrument.h
+++ SFMLTest02/Instrument.h
@@ -2,6 +2,7 @@
 #define DEF_Instrument
 
 #include <iostream>
+#include <SFML/Audio.hpp>
 #include <string>
 #include <map> 
 #include "Notes.h"
@@ -11,11 +12,15 @@ class Instrument
 {
 	public:
 		Instrument(std::string name);
-		Instrument(std::string name, std::map<int, Notes[42]>);
+		Instrument(std::string name, std::map<int, Notes> tabNotes);
 		~Instrument();
+		void addNotes(int emp, Notes noteSup);
+		void addNotes(int emp, std::string noteFile);
+		void addNotes(int emp, sf::SoundBuffer noteBuff);
+		void play(int emp);
 
 	private:
-		std::map<int, Notes [42]> listNotes;
+		std::map<int, Notes> listNotes;
 		std::string nameInstrument;
 };
 
